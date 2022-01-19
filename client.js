f7d321f679a8d450856e90377ed4dd7a75c88067
Neale Taylor <nealeptaylor@gmail.com>
2022-01-19 13:43:21 -0500

    Trial and errorred moving the snake around

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const net = require("net");
 
 
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: '192.168.2.59',
     port: 50541,
@@ -9,6 +9,15 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  conn.on("connect", (client) => {
+    console.log("Successsfully connected to game server");
+    conn.write("Name: NPT");
+    // conn.write("Move: up");
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 50);
+  });
 
   conn.on("data", data => {
     console.log(data);
