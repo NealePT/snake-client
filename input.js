25355897d8f2f9bca7b465b1de1326a7d7bea883
Neale Taylor <nealeptaylor@gmail.com>
2022-01-19 14:19:00 -0500

    added WASD movement

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = () => {
+const { connect } = require("./client");
+
+let connection;
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -7,6 +11,18 @@ const setupInput = () => {
   const handleUserInput = key => {
     if (key === '\u0003') {
       process.exit();
+    }
+    if (key === 'w') {
+      conn.write("Move: up");
+    }
+    if (key === 'a') {
+      conn.write("Move: left");
+    }
+    if (key === 's') {
+      conn.write("Move: down");
+    }
+    if (key === 'd') {
+      conn.write("Move: right");
     }
   };
   stdin.on("data", handleUserInput);
