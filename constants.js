ebe396440f69d2de107100012dd5f0b89c9d0d3b
Neale Taylor <nealeptaylor@gmail.com>
2022-01-19 14:59:07 -0500

    added constants.js

---
+++ constants.js
@@ -0,0 +1,25 @@
+const IP = '192.168.2.59';
+const PORT = 50541;
+const w = "Move: up";
+const a = "Move: left";
+const s = "Move: down";
+const d = "Move: right";
+const g = "Say: Good luck!";
+const h = "Say: Have fun!";
+const j = "Say: Out of my way!";
+
+const keyPress = {
+  w,
+  a,
+  s,
+  d,
+  g,
+  h,
+  j
+};
+
+module.exports = {
+  IP,
+  PORT,
+  keyPress
+};
