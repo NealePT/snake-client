6871cd2b4d5000e1e55c01e097086b427cc7c9f7
Neale Taylor <nealeptaylor@gmail.com>
2022-01-19 13:59:42 -0500

    refactored setUpInput into input.js

--- play.js
+++ play.js
@@ -1,21 +1,6 @@
 const { connect } = require("./client.js");
+const { setupInput } = require("./input.js");
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  const handleUserInput = key => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
-  stdin.on("data", handleUserInput);
-
-
-  return stdin;
-};
 
 console.log("Connecting ...");
 connect();
